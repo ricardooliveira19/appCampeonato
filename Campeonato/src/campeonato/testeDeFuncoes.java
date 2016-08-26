6dfb34da5d1c41dd29c8b2786501a303c8dece9d
ricardo.oliveira <ricardo.oliveira@SASPO-E00XPYQ-L.sa.cushwake.gbl>
2016-08-26 17:35:56 -0300

    Primeiras funções para criação do App

---
+++ Campeonato/src/campeonato/testeDeFuncoes.java
@@ -0,0 +1,15 @@
+package campeonato;
+
+import static campeonato.Campeonato.confrontMataMata;
+import static campeonato.Campeonato.n;
+import static campeonato.Campeonato.porPontosCorrido;
+import static campeonato.Campeonato.sorteioTimes;
+import static campeonato.Campeonato.timesEJogadores;
+
+/**
+ * @author ricardo.oliveira
+ */
+public class testeDeFuncoes {
+
+
+}
